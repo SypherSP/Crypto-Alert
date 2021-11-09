3eb778195109c6a7bf64c7d9753305c1c1f05234
ManasAg-hub <86635617+ManasAg-hub@users.noreply.github.com>
2021-11-09 12:11:50 +0530

    added refresh, addToList, modified remove, removed timer

--- src/main/java/com/example/crypto/CoinController.java
+++ src/main/java/com/example/crypto/CoinController.java
@@ -7,7 +7,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldListCell;
 
 import java.net.URL;
 import java.util.*;
@@ -17,56 +19,59 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class CoinController implements Initializable {
-//    private ScheduledExecutorService executorService;
-    @FXML
-    private TableColumn<GuiCoin, Double> price;
-    @FXML
-    private TableColumn<GuiCoin, String> name;
-    @FXML
-    private TableView<GuiCoin> table;
+//    configure the table
+    @FXML private TableColumn<Coin, Double> price;
+    @FXML private TableColumn<Coin, String> name;
+    @FXML private TableView<Coin> table;
 
+//    these instance variables are used to create new coins
+    @FXML private TextField symbolTextField;
+    @FXML private TextField nameTextField;
 
-    ObservableList<GuiCoin> myList = populateData();
+    ObservableList<Coin> myList = populateData();
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        name.setCellValueFactory(new PropertyValueFactory<GuiCoin, String>("name"));
-        price.setCellValueFactory(new PropertyValueFactory<GuiCoin, Double>("price"));
+        name.setCellValueFactory(new PropertyValueFactory<Coin, String>("CoinName"));
+        price.setCellValueFactory(new PropertyValueFactory<Coin, Double>("price"));
         table.setItems(myList);
     }
+
     @FXML
     private void deleteRowFromTable(ActionEvent event){
-        table.getItems().removeAll(table.getSelectionModel().getSelectedItem());
+        myList.remove(table.getSelectionModel().getSelectedItem());
     }
 
-    private ObservableList<GuiCoin> populateData(){
-        List<Coin> coinList= new ArrayList<>(4);
-        ObservableList<GuiCoin> myList = FXCollections.observableArrayList();
+    @FXML
+    private void refresh(ActionEvent event) {
+        for(int i=0;i<myList.size();i++) {
+            Coin temp = new Coin(myList.get(i).getSymbol(), myList.get(i).getCoinName());
+            temp.start();
+            myList.set(i,temp);
+        }
+        table.setItems(myList);
+    }
+
+    @FXML
+    private void addToList() {
+        Coin coinFromUser = new Coin(symbolTextField.getText(), nameTextField.getText());
+        coinFromUser.start();
+        myList.add(coinFromUser);
+    }
+
+    private ObservableList<Coin> populateData(){
+        ObservableList<Coin> myList = FXCollections.observableArrayList();
 
         //initializing coins
         //set lower and upper values from form when adding a coin
-        coinList.add(new Coin("BNBBUSD","Binance Coin"));
-        coinList.add(new Coin("BTCBUSD","Bitcoin"));
-        coinList.add(new Coin("ETHBUSD","Ethereum Coin"));
-        coinList.add(new Coin("SHIBBUSD","Shib Inu"));
+        myList.add(new Coin("BNBBUSD","Binance Coin"));
+        myList.add(new Coin("BTCBUSD","Bitcoin"));
+        myList.add(new Coin("ETHBUSD","Ethereum Coin"));
+//        myList.add(new Coin("SHIBBUSD","Shib Inu"));
 
-        for(Coin coin:coinList){
+        for(Coin coin:myList){
             coin.start(); //starting all threads
         }
-        for(Coin coin:coinList) {
-            if(coin.getPrice()!=null)
-            myList.add(new GuiCoin(coin.getName(), coin.getPrice()));
-        }
-        Timer t=new Timer();
-        t.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                for(int i=0;i<4;i++) {
-                    if(coinList.get(i).getPrice()!=null)
-                    myList.set(i, new GuiCoin(coinList.get(i).getCoinName(), coinList.get(i).getPrice()));
-                }
-            }
-        },0,2* 1000L);
-
         return myList;
     }
 }
